145d246fe5f496fb1b1478e2a233c25a88eac8e3
Cesar Costa <cesarcosta.augustos@gmail.com>
2020-02-15 18:15:40 -0300

    Alterado tipo de price em Task e total_price em Chronogram para Decimal_Field

--- confidence_chronograms/models.py
+++ confidence_chronograms/models.py
@@ -9,9 +9,9 @@ class Chronogram(models.Model):
     owner = models.CharField(max_length=80)
     address = models.CharField(verbose_name = 'Endereço', max_length=200)
     total_time = models.CharField(verbose_name = 'Tempo total', max_length=30)
-    total_price = models.CharField(verbose_name = 'Valor total', max_length=20)
+    total_price = models.DecimalField(verbose_name = 'Valor total', max_digits=10, decimal_places=4)
     date_added = models.DateTimeField(verbose_name = 'Data de criação', auto_now_add=True)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT)
 
     def __str__(self):
         """Devolve uma representação em string do modelo."""
@@ -27,8 +27,8 @@ class Chronogram(models.Model):
 class Task(models.Model):
     """Tarefa específica do cronograma."""
     chronogram = models.ForeignKey(Chronogram, on_delete=models.PROTECT)
-    task_text = models.TextField(verbose_name = 'Dscrição da Tarefa')
-    price = models.DecimalField(verbose_name = 'Valor', max_digits=10,decimal_places=4)
+    task_text = models.TextField(verbose_name = 'Descrição da Tarefa')
+    price = models.DecimalField(verbose_name = 'Valor', max_digits=10, decimal_places=4)
     start_date = models.DateTimeField(verbose_name = 'Inicio')
     end_date = models.DateTimeField(verbose_name = 'Termino')
     duration = models.CharField(verbose_name = 'Duração', max_length=30)
