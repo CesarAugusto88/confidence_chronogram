aa6c4fa340a61b79ba8926b8d977e5a8dced0858
CesarAugusto88 <cesarcosta.augustos@gmail.com>
2020-03-16 02:05:34 -0300

    Deploy heroku

--- confidence_chronogram/wsgi.py
+++ confidence_chronogram/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'confidencechronogram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'confidence_chronogram.settings')
 
 application = get_wsgi_application()
