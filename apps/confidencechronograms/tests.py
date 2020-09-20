58fb00156ba29447f0ca131303000a86669e8987
Cesar Costa <cesarcosta.augustos@gmail.com>
2020-09-20 17:10:06 -0300

    Começando a criar testes

--- apps/confidencechronograms/tests.py
+++ apps/confidencechronograms/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Cliente, Funcionario
+
+class Status_Code_Tests(TestCase):
+    def test_status_code_login(self):
+        response = self.client.get(reverse('login_user'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_status_code_home(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_status_code_contact(self):
+        response = self.client.get(reverse('contact'))
+        self.assertEqual(response.status_code, 200)
